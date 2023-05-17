c3c7feded935fc548a966f1f135a32fdd9c6e1ef
fhllowe <fredrick.ru@gmail.com>
2023-05-17 22:05:41 +0700

    added links to gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -11,13 +11,13 @@ const Gallery = () => {
                   <a href='https://patricia-cupcakes.netlify.app/' target='_blank' rel='noreferrer noopener'><p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p></a>
                  </div>
                  <div className={styles.galleryItem}>
-                  <p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p>
+                  <a href='https://jovial-flan-9254a7.netlify.app/' target='_blank' rel='noreferrer noopener'> <p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p> </a>
                  </div>
                  <div className={styles.galleryItem}>
-                  <p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p>
+                 <a href='https://impossible-erin-pumps.cyclic.app/espresso' target='_blank' rel='noreferrer noopener'><p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p></a>
                  </div>
                  <div className={styles.galleryItem}>
-                  <p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p>
+                 <a href='https://shiny-klepon-9af19a.netlify.app/' target='_blank' rel='noreferrer noopener'><p>Lorem ipsum keme keme keme 48 years Mike chapter quality control kemerloo sa na katol tamalis planggana shokot at nang jowa ma-kyonget at na na ang daki shonget ganda lang pranella ano at nang lulu Mike at bakit chipipay shonget bokot at bakit na ang majonders biway ang 48 years klapeypey-klapeypey na ang jutay ng nang at nang ano tetetet  </p> </a>
                  </div>
 
           </section>
